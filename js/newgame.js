b6f93272b0f2ea9c03645fc60e6d6530c84b1f6c
Marco Ribeiro <mribeirodev@gmail.com>
2016-12-08 13:10:32 -0200

    Fixing some problems in css classes and in the javascript

--- js/newgame.js
+++ js/newgame.js
@@ -1,15 +1,19 @@
-'use strict'
 ;((length) => {
+  'use strict'
   let table = document.createElement('table');
+  table.classList.add('table-game');
   for (let col = 1; col <= length; col++){
     let tr = document.createElement('tr');
     for (let row = 1; row <= length; row++ ){
       let td = document.createElement('td');
       td.id = String(col) + 'x' + String(row);
+      let block = document.createElement('div');
+      block.classList.add('block', 'closed');
+      td.appendChild(block);
       tr.appendChild(td);
     }
     table.appendChild(tr);
   }
   const gameboard = document.getElementById('gameboard');
   gameboard.appendChild(table);
-})(3);
+})(9);
