67329c889da695dbaa9aed77e14506e85a9b8ef4
Marco Ribeiro <mribeirodev@gmail.com>
2016-12-10 01:16:30 -0200

    revision to generate the new gameboard

--- js/newgame.js
+++ js/newgame.js
@@ -1,19 +1,28 @@
+function mineField () {
+  return [
+    ['b', 1,  ' '],
+    [1,   1,  ' '],
+    [' ',  ' ', ' ']
+  ]
+}
 ;((length) => {
   'use strict'
   let table = document.createElement('table');
   table.classList.add('table-game');
-  for (let col = 1; col <= length; col++){
+  const gameboardArray = mineField();
+  gameboardArray.forEach( (row, indexRow) => {
     let tr = document.createElement('tr');
-    for (let row = 1; row <= length; row++ ){
+    row.forEach( (col, indexCol) => {
       let td = document.createElement('td');
-      td.id = String(col) + 'x' + String(row);
       let block = document.createElement('div');
+      block.setAttribute('data-js', `${indexRow}x${indexCol}`);
+      block.setAttribute('data-value', col);
       block.classList.add('block', 'closed');
       td.appendChild(block);
       tr.appendChild(td);
-    }
+    });
     table.appendChild(tr);
-  }
+  });
   const gameboard = document.querySelector('div[data-js="gameboard"]');
-  gameboard.appendChild(table);  
+  gameboard.appendChild(table);
 })(9);
